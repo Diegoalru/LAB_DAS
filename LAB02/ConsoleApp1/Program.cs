5a654043d4606dcf34ef22d9ce03c7809c36e9f8
Diegoalru <43016665+Diegoalru@users.noreply.github.com>
2018-09-17 21:03:19 -0600

    LAB02DAS

---
+++ LAB02/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LAB02;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Person");
+
+              //LAB v1
+              Persona p = new Persona();
+              p.Id = 1;
+              p.Name = "Diego";
+              p.LastName = "Rubi";
+             
+
+            /* //LAB v2
+             * Persona p = new Persona(1, "Diego", "Rubi");
+             */
+
+            /* //LAB v3
+             * Persona p = new Persona
+             * {
+             * Id = 1,
+             * Name = "Diego",
+             * LastName = "Rubí"
+             * };
+             */
+
+
+            String personString = string.Format("ID: {0}, Name: {1} and Last Name: {2}.", p.Id, p.Name, p.LastName);
+
+            Console.WriteLine(personString);
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LAB02/LAB02/Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB02
+{
+    public class Persona
+    {
+
+        #region Variables
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        #endregion
+
+        
+        public Persona()
+        {}
+        
+
+        public Persona(int id, string name, string lastName)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+        }
+
+        
+
+    }
+}
